0b7c8bf7a7bc7991c64441ecfd876ea10dbcd78c
kemykemyskov <87675966+kemykemyskov@users.noreply.github.com>
2021-07-24 18:30:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const Twit = require('twit')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+var T = new Twit({
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
+
+
+
+client.login(process.env.DISCORD_TOKEN);
+client.once('ready', () => {
+  var stream = T.stream('statuses/filter', { follow: [process.env.TWITTER_USER_ID] })
+
+  stream.on('tweet', function (tweet) {
+    //only show owner tweets
+    if(tweet.user.id == process.env.TWITTER_USER_ID) {
+     
+      var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+     
+      const exampleEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(tweet.user.screen_name )
+      .setURL(url)
+      .setAuthor('@'+ tweet.user.screen_name , 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+      .setDescription(tweet.text)
+      ;
+     
+ //  var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+    try {
+        let channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel => {
+          //channel.send(url)
+          //channel.send("Rudy")
+          //console.log(channel)
+          channel.send(exampleEmbed);
+        }).catch(err => {
+          console.log(err)
+        })
+    } catch (error) {
+            console.error(error);
+      }
+    }
+  })
+})

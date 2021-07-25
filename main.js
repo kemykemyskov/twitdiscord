55705ed7937b5778ab33ec9460745fa2c4bce149
kemykemyskov <87675966+kemykemyskov@users.noreply.github.com>
2021-07-26 01:29:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,29 +18,35 @@ var T = new Twit({
 
 client.login(process.env.DISCORD_TOKEN);
 client.once('ready', () => {
-  var stream = T.stream('statuses/filter', { follow: [process.env.TWITTER_USER_ID] })
+  var stream = T.stream('statuses/filter', { follow: [ process.env.TWITTER_USER_ID] })
 
   stream.on('tweet', function (tweet) {
     //only show owner tweets
-    if(tweet.user.id == process.env.TWITTER_USER_ID) {
-     
+    if(tweet.user.id == ( process.env.TWITTER_USER_ID)) {
+     tweet.is_quote_status =true;
       var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
      
+
+      T.post('statuses/update', { status: '@SneaksFamily Discord: [Sneaks](www.discord.com) 📍 🇧🇪 / 🇫🇷  '+ url }, function(err, data, response) {
+        console.log(data)
+      })
+    console.log(tweet)
       const exampleEmbed = new Discord.MessageEmbed()
-      .setColor('#0099ff')
-      .setTitle(tweet.user.screen_name )
-      .setURL(url)
-      .setAuthor('@'+ tweet.user.screen_name , 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-      .setDescription(tweet.text)
+      .setColor('#ffb800')
+     // .setTitle(tweet.user.screen_name )
+     // .setURL('https://twitter.com/anadoluimages/status/1419165088263675906')
+      .setAuthor('@'+ tweet.user.screen_name , tweet.user.profile_image_url, url)
+      .setDescription('[@SneaksFamily](https://twitter.com/SneaksFamily) \n Discord: [SneaksFamily](www.discord.com) 📍 🇧🇪 / 🇫🇷  \n' + url)
+     // .setImage('https://pbs.twimg.com/media/E7ILScqXoAEL10Y.jpg')
+      .setTimestamp()
+      .setFooter("[@SneaksFamily](https://twitter.com/SneaksFamily) \n Discord: [SneaksFamily](www.discord.com) 📍 🇧🇪 / 🇫🇷  " + "\n", tweet.user.profile_image_url);
       ;
-     
- //  var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+      
+   
     try {
         let channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel => {
-          //channel.send(url)
-          //channel.send("Rudy")
-          //console.log(channel)
           channel.send(exampleEmbed);
+          console.log(exampleEmbed) ;
         }).catch(err => {
           console.log(err)
         })

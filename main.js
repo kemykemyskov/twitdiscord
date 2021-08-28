20c890b89fb58ae13c3601ce7a7d29976a747426
kemykemyskov <87675966+kemykemyskov@users.noreply.github.com>
2021-08-28 18:27:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const Twit = require('twit')
+var getImagesFromTweet = require('get-images-from-tweet');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 
 var T = new Twit({
   consumer_key:         process.env.TWITTER_CONSUMER_KEY,
@@ -13,46 +13,47 @@ var T = new Twit({
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
 
-
-
-
 client.login(process.env.DISCORD_TOKEN);
 client.once('ready', () => {
-  var stream = T.stream('statuses/filter', { follow: [ process.env.TWITTER_USER_ID] })
+    T.get('friends/ids', { screen_name: 'kemyskov'},  function (err, friends, response) {
+        var stream = T.stream('statuses/filter', { follow: friends.ids });
+        stream.on('tweet', function (tweet) {
+            var str = tweet.text;
+            var str_low = str.toLowerCase();  
+            var reg = /(post|publication)/g;  
+                if (reg.test(str_low)){
+                    var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+                    T.post('statuses/update', { status: '@SneaksFamily Discord: [Sneaks](www.discord.com) 📍 🇧🇪 / 🇫🇷  '+ url }, function(err, data, response) { })
+                    txt = str.replace(str.substr( str.length - 23 ),'');
 
-  stream.on('tweet', function (tweet) {
-    //only show owner tweets
-    if(tweet.user.id == ( process.env.TWITTER_USER_ID)) {
-     tweet.is_quote_status =true;
-      var url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
-     
-
-      T.post('statuses/update', { status: '@SneaksFamily Discord: [Sneaks](www.discord.com) 📍 🇧🇪 / 🇫🇷  '+ url }, function(err, data, response) {
-        console.log(data)
-      })
-    console.log(tweet)
-      const exampleEmbed = new Discord.MessageEmbed()
-      .setColor('#ffb800')
-     // .setTitle(tweet.user.screen_name )
-     // .setURL('https://twitter.com/anadoluimages/status/1419165088263675906')
-      .setAuthor('@'+ tweet.user.screen_name , tweet.user.profile_image_url, url)
-      .setDescription('[@SneaksFamily](https://twitter.com/SneaksFamily) \n Discord: [SneaksFamily](www.discord.com) 📍 🇧🇪 / 🇫🇷  \n' + url)
-     // .setImage('https://pbs.twimg.com/media/E7ILScqXoAEL10Y.jpg')
-      .setTimestamp()
-      .setFooter("[@SneaksFamily](https://twitter.com/SneaksFamily) \n Discord: [SneaksFamily](www.discord.com) 📍 🇧🇪 / 🇫🇷  " + "\n", tweet.user.profile_image_url);
-      ;
-      
-   
-    try {
-        let channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel => {
-          channel.send(exampleEmbed);
-          console.log(exampleEmbed) ;
-        }).catch(err => {
-          console.log(err)
+                    const exampleEmbed = new Discord.MessageEmbed()
+                    .setColor('#ffb800')
+                    .setAuthor('@'+ tweet.user.screen_name , tweet.user.profile_image_url, url)
+                    .addFields(
+                      { name: '__Twitter moniteur__ ', value: '[@SFbot](https://twitter.com/SFbot)', inline: true },
+                      { name: '__Twitter__ ', value: '[@SneaksFamily](https://twitter.com/SneaksFamily)', inline: true },
+                      { name: '__Discord__ ', value: '[@SneaksFamily](https://discord.gg/dRAWYbjhcE)', inline: true }
+                  )
+                   // .addField('\u200b', '\u200b')
+                    .setDescription('\n' + txt + '\n ')
+                    .setImage(getImagesFromTweet(tweet).toString())
+                    .setTimestamp()
+                    .setFooter("Twitter : @SneaksFamily \nDiscord : https://discord.gg/dRAWYbjhcE \n📍 🇧🇪 / 🇫🇷 ", tweet.user.profile_image_url);
+                    ;
+                    
+                  try {
+                      let channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel => {
+                        channel.send(exampleEmbed);
+              
+                      }).catch(err => {
+                        console.log(err)
+                      })
+                  } catch (error) {
+                          console.error(error);
+                    }
+                }else{
+                    console.log("nope")
+                }
         })
-    } catch (error) {
-            console.error(error);
-      }
-    }
-  })
+    })
 })
